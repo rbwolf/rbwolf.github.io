57b46ba2235b51ba91425e24444f0f83c1191794
Ryan Baerwolf <rdbaerwolf@gmail.com>
2021-03-09 13:38:35 -0700

    Replace with simple CRA; header bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap-css-only'
+import React from 'react'
+import Header from './components/Header'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className={'container'}>
+      <Header/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import 'bootstrap-css-only'
+import { ReactComponent as Mail } from '../svg/mail.svg'
+import { ReactComponent as GitHub } from '../svg/github.svg'
+import { ReactComponent as LinkedIn } from '../svg/linkedin.svg'
+import './Header.css'
+
+const Header = () => {
+
+  const headerActions = [
+    {
+      icon: <Mail/>,
+      link: 'mailto://rdbaerwolf@gmail.com'
+    },
+    {
+      icon: <LinkedIn/>,
+      link: 'https://linkedin.com/in/ryanbaerwolf'
+    },
+    {
+      icon: <GitHub/>,
+      link: 'https://github.com/'
+    }
+  ]
+
+  return (
+    <div className={'header-wrapper py-4'}>
+
+      {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+      <h5 className={'font-weight-bold'}>// rbwolf</h5>
+
+      <div className={'d-flex'}>
+        {headerActions.map(action =>
+          <a href={action.link}
+             className={'ml-4 icon'}>
+            {action.icon}
+          </a>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Header
