8c2f0626e6aeb32c4cb13fcde5603a03dafa5cff
rbwolf <rdbaerwolf@gmail.com>
2022-01-23 14:37:42 -0800

    Proper repo name for deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/rbwolf/spades.git master:deploy
+git push -f https://github.com/rbwolf/rbwolf.github.io.git master:deploy
 
 cd -
